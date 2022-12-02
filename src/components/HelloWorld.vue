b0c39fa8f3a1af49ac32e409b6cc68c07c983865
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-12-02 16:28:44 +0500

    oq-uz-app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 export default {
-  
+  name: 'ApHelloWorld'
 }
 </script>
 <style lang="HelloWorld">
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template lang="Navbar">
+    <div>
+        navabr
+        <button v-on:click="display">Alert</button>
+
+        <h3 v-if="show">Conditional true</h3>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ApNavbar',
+    methods: {
+        hello() {
+            alert("HELLOG")
+        },
+        display() {
+            this.show = !this.show
+        }
+    },
+    data() {
+        return {
+            show: true
+        }
+    },
+}
+</script>
+<style lang="Navbar">
+    
+</style>
